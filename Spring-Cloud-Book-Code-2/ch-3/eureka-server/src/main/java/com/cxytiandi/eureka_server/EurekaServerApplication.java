aeedf641fe733efc2f28806ec030e012623b5e2c
wuyf <windsnow263@163.com>
2019-08-29 15:52:57 +0800

    取消了Eureka验证

--- Spring-Cloud-Book-Code-2/ch-3/eureka-server/src/main/java/com/cxytiandi/eureka_server/EurekaServerApplication.java
+++ Spring-Cloud-Book-Code-2/ch-3/eureka-server/src/main/java/com/cxytiandi/eureka_server/EurekaServerApplication.java
@@ -3,10 +3,6 @@ package com.cxytiandi.eureka_server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 
 /**
@@ -27,16 +23,16 @@ public class EurekaServerApplication {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 	
-	@Configuration
-	@EnableWebSecurity
-	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	    @Override
-	    protected void configure(HttpSecurity http) throws Exception {
-	    	//关闭csrf
-	        http.csrf().disable();
-	        // 支持httpBasic
-	        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
-	    }
-	}
+//	@Configuration
+//	@EnableWebSecurity
+//	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//	    @Override
+//	    protected void configure(HttpSecurity http) throws Exception {
+//	    	//关闭csrf
+//	        http.csrf().disable();
+//	        // 支持httpBasic
+//	        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+//	    }
+//	}
 	
 }
